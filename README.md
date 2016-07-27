999208446a8d1186a8bb3cdb57501417a88dfb29
wxhl <wangch6688@hotmail.com>
2016-07-27 15:26:42 +0800

    loop in swift

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 # 1. Swift_01   write the basic grammar
 
 # 2. Swift_02   write operational character about swift and the ways about use if and switch in swift  
+
+# 2. Swift_02   write the ways to loop in swift  (while   repeat while)
---
+++ Swift——03.playground/Contents.swift
@@ -0,0 +1,56 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//while
+
+var i = 0
+
+while i < 10{
+    print(i)
+    
+    i++
+}
+
+
+//repeat-while
+var j = 10
+
+repeat {
+
+j++
+print(j)
+
+}while j < 20
+
+
+
+//For ---swift3.0之后就不能使用了
+
+for i = 0;i < 5;i++ {
+    
+    print(i)
+    
+}
+
+//for...in
+//范围运算符
+//遍历0-10
+var k = 0
+for k in 0...10{
+    
+    print(k)
+}
+
+//0..<10  --->[0,10)
+var x = 0
+for x in 0..<10{
+    
+    print(x)
+}
+
+
+
+
+
+
