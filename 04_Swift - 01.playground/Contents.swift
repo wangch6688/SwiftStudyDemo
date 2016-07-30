8708022f0ace4b5c3fa08ea009c6c1a45e567ec4
wxhl <wangch6688@hotmail.com>
2016-07-30 16:45:20 +0800

    some change

--- 04_Swift - 01.playground/Contents.swift
+++ 04_Swift - 01.playground/Contents.swift
@@ -1,4 +1,5 @@
 //: Playground - noun: a place where people can play
+import Foundation
 
 
 //1.闭包 -- 结构
@@ -27,3 +28,12 @@ names.sort ({ (s1 :String, s2 :String) -> Bool in
     
        return s1 > s2
 })
+//playgtound 查看各种算法
+//评判算法的好坏： 时间复杂度（运算次数，运算的事件）  ，空间复杂度
+
+
+
+
+
+
+
